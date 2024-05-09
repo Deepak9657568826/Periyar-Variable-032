44cd6e5fcbea0f74dcd5fe57d854e7121e4e92e8
Deepak Wayzode <dwayzode@gmail.com>
2024-05-09 11:23:50 +0530

    login signup component complete

--- frontend/skillify/src/component/Navbar.jsx
+++ frontend/skillify/src/component/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import projectLogo from "../assets/projectLogo.jpg"
 
-function Navbar() {
+
+function Navbar(){
     return (
         <div className="bg-gray-800 text-white py-4">
             <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-xl font-bold">Home</Link>
+                {/* Replace "Home" text with your website logo */}
+                <Link to="/">
+                    <img src={projectLogo} alt="Website Logo" className="h-8" />
+                </Link>
                 <div className="flex space-x-4">
+                    <Link to="/" className="hover:text-gray-300">Home</Link>
                     <Link to="/about" className="hover:text-gray-300">About Us</Link>
                     <Link to="/contact" className="hover:text-gray-300">Contact Us</Link>
                     <Link to="/services" className="hover:text-gray-300">Services</Link>
@@ -17,5 +23,4 @@ function Navbar() {
         </div>
     );
 }
-
 export default Navbar;
