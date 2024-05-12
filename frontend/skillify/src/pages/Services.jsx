38b407ef6099de082bdb7dbcc3c0ba00968b36a7
Deepak Wayzode <dwayzode@gmail.com>
2024-05-12 22:43:47 +0530

    Merge branch 'main' of https://github.com/Deepak9657568826/Periyar-Variable-032 into fw28_098_day_3

--- frontend/skillify/src/pages/Services.jsx
+++ frontend/skillify/src/pages/Services.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import '../styles/servicepage.css';
 import { GrFavorite } from "react-icons/gr";
 import ServiceDescription from './ServiceDescription';
+
 function Services() {
+
+  const [services, setServices] = useState([]);
+  const url = "https://gray-xerus-sock.cyclic.app/newServices";
+
+  function fetchData() {
+    fetch(url)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        setServices(data.offerServices);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
-
+<div>
+        <h2 className='text-3xl font-bold mt-10 ml-10'> All Services</h2>
+      </div>
       <div>
+        <ul className="cards">
+          {services.map((service) => (
+            <li key={service._id}>
+              <a href="#" className="card">
+                <img src={service.backgroundPhoto} className="card__image" alt="" />
+                <h2 className="card__tagline"><GrFavorite color='white'  /></h2>
+                <div className="card__overlay">
+                  <div className="card__header">
+                    {/* Removed SVG element */}
+                    <img className="card__thumb" src={service.profilePhoto} alt="" />
+                    <div className="card__header-text">
+                      <h3 className="card__title">{service.nameOfUser}</h3>
+                      <span className="card__status">{service.timeAvailable}</span>
+                    </div>
+                  </div>
+                  <p className="card__description">{service.detailsDescription}</p>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
         <ul className="cards">
           <li>
             <a href="" className="card">
@@ -30,9 +73,9 @@ function Services() {
           </li>
         </ul>
       </div>
-      {/* <ServiceDescription /> */}
+      {/* <ServiceDescription  /> */}
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;
